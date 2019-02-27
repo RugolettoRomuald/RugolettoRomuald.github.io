18092107ddb93d1f2935f6d45346bed78c8b321d
FToXik <vuranib@gmail.com>
2019-02-27 11:19:40 +0100

    Open links in a new tab

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,15 +1,20 @@
-function stopVideos(){
-	var videos = document.getElementsByTagName("video");
-	
-	for(var i = 0; i < videos.length; i++) { 
-		videos[i].pause();
-		videos[i].currentTime = 0;
-		videos[i].load();
-	}
-}
-
 $(document).ready(function(){
+	// Stop and reload video on modal close
 	$(".portfolio-modal-dismiss").on('click', function(){
-		stopVideos();
+		var videos = document.getElementsByTagName("video");
+		
+		for(var i = 0; i < videos.length; i++) { 
+			videos[i].pause();
+			videos[i].currentTime = 0;
+			videos[i].load();
+		}
+	});
+	
+	// Open mailto links in a new tab
+	$("a[href^='mailto:']").click(function(e) {
+		e.preventDefault();
+		var href = $(this).attr('href');
+		var target = $(this).attr('target');
+		window.open(href, target ? target : '_self');
 	});
 });
