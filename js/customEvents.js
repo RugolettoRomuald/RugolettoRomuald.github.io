b6bbacdd17456be65e7c9f16b320d6ac50193d8b
FToXik <vuranib@gmail.com>
2019-03-11 17:47:28 +0100

    Add english version
    + Automatically translated according to the browser language
    + Add english subtitles on ram and perfectMatch
    + Add english version of my cv

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
-	// Stop and reload video on modal close
-	$(".portfolio-modal-dismiss").on('click', function(){
-		var videos = document.getElementsByTagName("video");
-		
-		for(var i = 0; i < videos.length; i++) { 
-			videos[i].pause();
-			videos[i].currentTime = 0;
-			videos[i].load();
-		}
-	});
-	
+$(document).ready(function()
+{	
 	// Open mailto links in a new tab
 	$("a[href^='mailto:']").click(function(e) {
 		e.preventDefault();
 		var href = $(this).attr('href');
 		var target = $(this).attr('target');
 		window.open(href, target ? target : '_self');
+	});
+	
+	// Force translate
+	$("#lang-selector").click(function(e) {
+		e.preventDefault();
+		var currentLang = $("#lang-selector").html()
+		$.translate((currentLang == "fr")? "en" : "fr");
+		$("#lang-selector").blur();
 	});
 });
